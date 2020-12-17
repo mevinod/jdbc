f16837423b48cb582d1ed3480e7907778652315d
vinod-1 <vinod@plank.life>
2020-12-17 15:22:49 +0530

    Sample JDBC project created

---
+++ src/main/java/PostgreSQLJDBC.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class PostgreSQLJDBC {
+    public static void main( String args[] ) {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://192.168.0.112:5432/db",
+                            "uname", "paswd");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT count (*) FROM wallet;" );
+            rs.next();
+            int recordCount = rs.getInt(1);
+            System.out.println("Total record count is - "+recordCount);
+            if (recordCount > 50000) {
+                System.out.println("Record count is more than 50K");
+            }
+            else
+                System.out.println("Record count is less than 50K");
+
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+    }
+}
